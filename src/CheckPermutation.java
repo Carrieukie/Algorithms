4b2bc3c5d8df053ca2b4355c102e6a70a45a3256
Carrieukie <kariuki931@gmail.com>
2022-01-11 10:58:50 +0300

    More challenges

--- src/CheckPermutation.java
+++ src/CheckPermutation.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
  * <p>
@@ -19,11 +21,31 @@
 public class CheckPermutation {
 
     public static void main(String[] args) {
-
+        System.out.println(checkInclusion("hello", "ooolkdnckalsdnvcojsandvcjawheollnsdcvjansldcsjdbviewjvbasdvsavkjsdv"));
     }
 
     public static boolean checkInclusion(String s1, String s2) {
 
-        return false;
+        if (s2.length() < s1.length() || s2.length() == 0) return false;
+        if (s1.length() == 0) return true;
+
+        int x = s1.length(), y = s2.length();
+        int[] array1 = new int[26];
+        int[] array2 = new int[26];
+
+        for (int i = 0; i < x; i++) {
+            array1[s1.charAt(i) - 'a']++;
+            array2[s2.charAt(i) - 'a']++;
+
+        }
+
+        for (int i = x; i < y; i++) {
+            if (Arrays.equals(array1, array2)) {
+                return true;
+            }
+            array2[s2.charAt(i - x) - 'a']--;
+            array2[s2.charAt(i) - 'a']++;
+        }
+        return Arrays.equals(array1, array2);
     }
 }

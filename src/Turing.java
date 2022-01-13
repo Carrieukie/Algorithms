31b933c5c9b0a3814ace002ed6349b77c3fa8ce6
Carrieukie <kariuki931@gmail.com>
2022-01-13 11:58:08 +0300

    More challenges

--- src/Turing.java
+++ src/Turing.java
@@ -17,27 +17,27 @@ public class Turing {
     }
 
 
-    //    public static int calcpoints(String[] ops){
-//        ArrayList<Integer> points = new ArrayList<>();
-//
-//        for (String string : ops){
-//            if(string.matches("-?\\d+")){
-//                points.add(Integer.valueOf(string));
-//            }
-//            if (string.equals("+")){
-//                points.add(points.get(points.size()-1) + points.get(points.size()-2));
-//            }
-//            if (string.equals("D")){
-//                points.add(2 * points.get( points.size()-1));
-//            }
-//            if (string.equals("C")){
-//                points.remove(points.size()-1);
-//            }
-//        }
-//        int ans = 0;
-//        for (Integer integer: points){
-//            ans += integer;
-//        }
-//        return ans;
-//    }
+        public static int calcpoints(String[] ops){
+        ArrayList<Integer> points = new ArrayList<>();
+
+        for (String string : ops){
+            if(string.matches("-?\\d+")){
+                points.add(Integer.valueOf(string));
+            }
+            if (string.equals("+")){
+                points.add(points.get(points.size()-1) + points.get(points.size()-2));
+            }
+            if (string.equals("D")){
+                points.add(2 * points.get( points.size()-1));
+            }
+            if (string.equals("C")){
+                points.remove(points.size()-1);
+            }
+        }
+        int ans = 0;
+        for (Integer integer: points){
+            ans += integer;
+        }
+        return ans;
+    }
 }

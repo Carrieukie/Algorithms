60419f22243c6c5b25a1cd2e10e6d09365d20dbe
Carrieukie <kariuki931@gmail.com>
2022-01-27 20:05:11 +0300

    Palindrome reorder

---
+++ src/Matrix.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+
+/**
+ * Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+ *
+ * The distance between two adjacent cells is 1.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+ * Output: [[0,0,0],[0,1,0],[0,0,0]]
+ * Example 2:
+ *
+ *
+ * Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+ * Output: [[0,0,0],[0,1,0],[1,2,1]]
+ */
+public class Matrix {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(updateMatrix(new int[][]{{0, 0, 0}, {0, 1, 0}, {1, 1, 1}})));
+    }
+
+    public static int[][] updateMatrix(int[][] mat) {
+        int r = mat.length, c = mat[0].length;
+
+        int[][] dis = new int[r][c];
+
+        for(int i = 0; i < r; i++)
+            for(int j = 0; j < c; j++)
+                if(mat[i][j] == 0) {
+                    explore(mat, dis, dis[i][j], i+1, j);
+                    explore(mat, dis, dis[i][j], i-1, j);
+                    explore(mat, dis, dis[i][j], i, j+1);
+                    explore(mat, dis, dis[i][j], i, j-1);
+                }
+
+        return dis;
+    }
+
+    public static void explore(int[][] mat, int[][] dis, int wt, int i, int j) {
+        if(i < 0 || j < 0 || i >= mat.length || j >= mat[0].length || mat[i][j] != 1)
+            return;
+
+        mat[i][j] = 2;
+
+        if(dis[i][j] == 0 || wt+1 < dis[i][j]) {
+            dis[i][j] = wt+1;
+            explore(mat, dis, wt+1, i+1, j);
+            explore(mat, dis, wt+1, i-1, j);
+            explore(mat, dis, wt+1, i, j+1);
+            explore(mat, dis, wt+1, i, j-1);
+        }
+
+        mat[i][j] = 1;
+    }
+
+}
--- src/PopulateRightPointersEachNode.java
+++ src/PopulateRightPointersEachNode.java
@@ -1,32 +1,87 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+ *
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+ *
+ * Initially, all next pointers are set to NULL.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,2,3,4,5,6,7]
+ * Output: [1,#,2,3,#,4,5,6,7,#]
+ * Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+ * Example 2:
+ *
+ * Input: root = []
+ * Output: []
+ */
 public class PopulateRightPointersEachNode {
     public static void main(String[] args) {
 
     }
 
-//    public Node connect(Node root) {
-//
-//        Queue<Node> queue = new LinkedList<>();
-//        queue.offer(root);
-//
-//        while (!queue.isEmpty()){
-//            int counter = children;
-//            //add layer children
-//            while (counter > 0){
-//                Node currentNode = queue.remove();
-//                queue.add(currentNode.left);
-//                queue.add(currentNode.right);
-//                counter --;
-//            }
-//            children *= 2;
-//
-//        }
-//
-//
-//        return root;
-//    }
+    //    This is a bfs approach
+    public Node connect(Node root) {
+
+        if (root == null) return null;
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            Node prev = null;
+
+            for (int i = 0; i < size; i++) {
+                Node node = q.poll();
+
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+
+                if (prev != null) {
+                    prev.next = node;
+                }
+
+                prev = node;
+            }
+        }
+
+        return root;
+    }
+
+    //    This is a more elegant DFS approach
+    public Node connectDfs(Node root) {
+        dfs(root);
+        return root;
+    }
+
+    void dfs(Node root) {
+        if (root == null) return;
+
+        if (root.left != null && root.right != null) {
+            if (root.next != null) {
+                root.right.next = root.next.left;
+            } else {
+                root.right.next = null;
+            }
+            root.left.next = root.right;
+        }
+
+        dfs(root.left);
+        dfs(root.right);
+    }
 
 }
 
@@ -36,7 +91,8 @@ class Node {
     public Node right;
     public Node next;
 
-    public Node() {}
+    public Node() {
+    }
 
     public Node(int _val) {
         val = _val;
